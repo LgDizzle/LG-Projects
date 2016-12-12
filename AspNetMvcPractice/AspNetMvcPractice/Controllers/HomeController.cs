c968d426a8cf3c9623434a4b49f7b3ed92b1aecb
Lauren <lauren.meen@gmail.com>
2016-12-11 20:28:55 -0600

    new stuff - drop downs still broken :s

---
+++ AspNetMvcPractice/AspNetMvcPractice/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvcPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Msg = "Hello World";
+            return View();
+        }
+
+        public ActionResult UnderConstruction()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
